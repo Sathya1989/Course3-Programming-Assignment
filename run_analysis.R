38b2f008865d9ead2d931c4a5ecf3dd0a5e30933
Sathya1989 <sathyamadhusudhanan@gmail.com>
2016-06-30 18:21:57 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
-Please include UCI_HAR_dataset in your working directory
-
 tidy_ds <- function(){
+##Please include UCI_HAR_dataset in your working directory
 ## Read all the data files
 test_data <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
 train_data <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
